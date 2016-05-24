8d59b5a33ad982744e4656ce611c15f9f258d0c4
atreto <alberto.treto@tklabs.com>
2016-05-24 11:39:52 -0400

    QUAL-67

--- src/qual/modules/rs232/rs232.py
+++ src/qual/modules/rs232/rs232.py
@@ -1,98 +1,114 @@
 from threading import Thread
 import serial
+import time
 
 from src.common.gpb.python.baseMessage import BaseMessage
 from src.common.module.module import Module
 
-match = 0
-mismatch = 0
-
 class StartMessage(BaseMessage):
     def __init__(self):
         super(StartMessage, self).__init__()
+        pass
 
 class StopMessage(BaseMessage):
     def __init__(self):
         super(StopMessage, self).__init__()
+        pass
 
 class RequestReportMessage(BaseMessage):
     def __init__(self):
         super(RequestReportMessage, self).__init__()
+        pass
 
 class StatusRequestMessage(BaseMessage):
     def __init__(self, match, mismatch):
+        self.match = match
+        self.mismatch = mismatch
         super(StatusRequestMessage, self).__init__()
+        pass
+
+class PortMsg():
+    def __init__(self, writer, reader):
+        self.writer = writer
+        self.reader = reader
 
 class Rs232(Module):
     def __init__(self, config={}):
         super(Rs232, self).__init__(config)
-
-class SerialReceiver(Thread):
-    def __init__(self, config={}):
-        Thread.__init__(self)
-        self.com = serialStr
+        self.addMsgHandler(StartMessage, self.start)
+        self.addMsgHandler(StopMessage, self.stop)
+        self.addMsgHandler(RequestReportMessage, self.report)
+        self.addThread(self.rs232Write())
+        self.addThread(self.rs232Read())
         self.match = 0
         self.mismatch = 0
-        self.ser = serial.Serial(
-            port=serialStr,
-            baudrate=115200,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS,
-            xonxoff=8
+
+    @classmethod
+    def getConfigurations(cls):
+        return [
+                {'port': '/dev/ttyUSB1', 'baudrate': 115200, 'parity': serial.PARITY_NONE, 'stopbits': serial.STOPBITS_ONE, 'bytesize': serial.EIGHTBITS},
+                {'port': '/dev/ttyUSB2', 'baudrate': 115200, 'parity': serial.PARITY_NONE, 'stopbits': serial.STOPBITS_ONE, 'bytesize': serial.EIGHTBITS},
+                {'port': '/dev/ttyUSB3', 'baudrate': 115200, 'parity': serial.PARITY_NONE, 'stopbits': serial.STOPBITS_ONE, 'bytesize': serial.EIGHTBITS},
+                {'port': '/dev/ttyUSB4', 'baudrate': 115200, 'parity': serial.PARITY_NONE, 'stopbits': serial.STOPBITS_ONE, 'bytesize': serial.EIGHTBITS},
+                {'port': '/dev/ttyUSB5', 'baudrate': 115200, 'parity': serial.PARITY_NONE, 'stopbits': serial.STOPBITS_ONE, 'bytesize': serial.EIGHTBITS},
+                {'port': '/dev/ttyUSB6', 'baudrate': 115200, 'parity': serial.PARITY_NONE, 'stopbits': serial.STOPBITS_ONE, 'bytesize': serial.EIGHTBITS},
+                ]
+
+    def rs232Write(self):
+        ser = serial.Serial(
+            port=self.portWriter,
+            baudrate=self.baudrate,
+            parity=self.parity,
+            stopbits=self.stopbits,
+            bytesize=self.bytesize
         )
-    def processData(self):
-        global mismatch
-        global match
+        character = chr(0)
+        while True:
+            ser.write(character)
+            time.sleep(0.5)
+            if ord(character) < 255:
+                character = chr(ord(character) + 1)
+            else:
+                character = chr(0)
+
+    def rs232Read(self):
+        ser = serial.Serial(
+            port=self.portReader,
+            baudrate=self.baudrate,
+            parity=self.parity,
+            stopbits=self.stopbits,
+            bytesize=self.bytesize
+        )
         character = chr(0)
         count = 0
         while True:
-            read = self.ser.read(1)
+            read = ser.read(1)
             if ord(read) != ord(character):
-                dif = ord(read)-count
+                dif = ord(read) - count
                 character = read
-                mismatch += abs(dif)
-                print "Test count=" + str(match + mismatch) + ", Mismatch: " + str(read)
+                self.mismatch += abs(dif)
             else:
-                match += 1
-                print "Test count=" + str(match + mismatch) + ", Match: " + str(read)
+                self.match += 1
             if ord(character) < 255:
                 character = chr(ord(character) + 1)
-                count += 1
             else:
                 character = chr(0)
-                count = 0
 
-    def run(self):
-        self.processData()
+    def start(self, msg):
+        self.portWriter = msg.writer
+        self.portReader = msg.reader
+        self.baudrate = self.config['baudrate']
+        self.parity= self.config['parity']
+        self.stopbits = self.config['stopbits']
+        self.bytesize = self.config['bytesize']
+        super(Rs232, self).startThread()
+        status = StatusRequestMessage(self.match, self.mismatch)
+        return status
 
+    def stop(self):
+        status = StatusRequestMessage(self.match, self.mismatch)
+        super(Rs232, self).stopThread()
+        return status
 
-class Serialsender(Thread):
-    def __init__(self, serialStr='0'):
-        Thread.__init__(self)
-        self.serialStr = serialStr
-        self.match = 0
-        self.mismatch = 0
-        self.ser = serial.Serial(
-            port=serialStr,
-            baudrate=115200,
-            parity=serial.PARITY_NONE,
-            stopbits=serial.STOPBITS_ONE,
-            bytesize=serial.EIGHTBITS,
-            xonxoff=1
-        )
-    def sendData(self):
-        character = chr(0)
-        while True:
-            self.ser.write(character)
-            character = chr(ord(character)+1)
-            if ord(character) >= 255:
-                character = chr(0)
-    def run(self):
-        self.sendData()
-
-serReceiver = SerialReceiver(serialStr='/dev/ttyUSB5')
-serReceiver.start()
-
-serSender = Serialsender(serialStr='/dev/ttyUSB6')
-serSender.start()
+# serReceiver = SerialReceiver(serialStr='/dev/ttyUSB5')
+# serReceiver.start()
