db894811621b86a24bb9dab72d65a1c2a1051deb
Brett Gilmer <brett.gilmer@tklabs.com>
2016-05-24 21:32:30 -0400

    QUAL-116 : Added unit test

--- src/common/logger/logger.py
+++ src/common/logger/logger.py
@@ -11,6 +11,15 @@ DEBUG = logging.DEBUG
 NOTSET = logging.NOTSET
 
 
+#Level	    When it is used
+#DEBUG	    Detailed information, typically of interest only when diagnosing problems.
+#INFO	    Confirmation that things are working as expected.
+#WARNING	An indication that something unexpected happened,
+#           or indicative of some problem in the near future (e.g. disk space low).
+#           The software is still working as expected.
+#ERROR	    Due to a more serious problem, the software has not been able to perform some function.
+#CRITICAL	A serious error, indicating that the program itself may be unable to continue running.
+
 ## Logger Class
 #
 # The logger class is derived from the standard logger.
