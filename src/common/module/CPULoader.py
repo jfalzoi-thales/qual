45daf4fa82ce20cf328f0d2bbf2c27a9e2dab22c
Christopher Wallace <chris.wallace@tklabs.com>
2016-05-25 12:35:53 -0400

    QUAL-16,QUAL-17 : Added comments and fixed imports

--- src/common/module/CPULoader.py
+++ src/common/module/CPULoader.py
@@ -1,65 +1,68 @@
 from time import sleep
 import threading
+
 lock = threading.Lock()
 
 ## CPULoader Class
 #
 class CPULoader(threading.Thread):
+    ## Constructor
+    #  @param     self
+    #  @param     sleeptime     number of seconds to sleep DEFAULT = [1]
     def __init__(self, sleeptime = 1):
+        ## initializes threading
         threading.Thread.__init__(self)
+        ## stores sleeptime in seconds
         self.sleeptime = sleeptime
+        ## initializes cpuload
         self.cpuload = {}
+        ## thread exits when self.quit = True
         self.quit = False
 
+    ## Grabs CPU usage statistics from Linux
+    #
+    #  Retreives CPU usage information from '/proc/stat' and parses necessary information
+    #
+    #  @param     self
+    #  @return    cpu_infos     dictionary containing CPU readings
     def getcputime(self):
-        '''
-        http://stackoverflow.com/questions/23367857/accurate-calculation-of-cpu-usage-given-in-percentage-in-linux
-        read in cpu information from file
-        The meanings of the columns are as follows, from left to right:
-            0cpuid: number of cpu
-            1user: normal processes executing in user mode
-            2nice: niced processes executing in user mode
-            3system: processes executing in kernel mode
-            4idle: twiddling thumbs
-            5iowait: waiting for I/O to complete
-            6irq: servicing interrupts
-            7softirq: servicing softirqs
+        cpu_infos = {}
 
-        #the formulas from htop 
-             user    nice   system  idle      iowait irq   softirq  steal  guest  guest_nice
-        cpu  74608   2520   24433   1117073   6176   4054  0        0      0      0
-
-
-        Idle=idle+iowait
-        NonIdle=user+nice+system+irq+softirq+steal
-        Total=Idle+NonIdle # first line of file for all cpus
-
-        CPU_Percentage=((Total-PrevTotal)-(Idle-PrevIdle))/(Total-PrevTotal)
-        '''
-        cpu_infos = {} #collect here the information
         with open('/proc/stat', 'r') as f_stat:
             lines = [line.split(' ') for content in f_stat.readlines() for line in content.split('\n') if line.startswith('cpu')]
 
-            #compute for every cpu
             for cpu_line in lines:
-                if '' in cpu_line: cpu_line.remove('')#remove empty elements
-                cpu_line = [cpu_line[0]]+[float(i) for i in cpu_line[1:]]#type casting
+                if '' in cpu_line: cpu_line.remove('')
+                cpu_line = [cpu_line[0]]+[float(i) for i in cpu_line[1:]]
                 cpu_id,user,nice,system,idle,iowait,irq,softrig,steal,guest,guest_nice = cpu_line
 
                 Idle = idle + iowait
                 NonIdle = user + nice + system + irq + softrig + steal
 
                 Total = Idle + NonIdle
-                #update dictionary
                 cpu_infos.update({cpu_id:{'total':Total,'idle':Idle}})
+
             return cpu_infos
 
+    ## Returns CPU Load information
+    #
+    #  Polls thread for cpuload once lock has been acquired
+    #
+    #  @param     self
+    #  @return    load  dict containing CPU load information
     def getcpuload(self):
         lock.acquire()
         load = self.cpuload
         lock.release()
         return load
 
+    ## Calculates CPU Load
+    #
+    #  Overrides run() method in Thread
+    #
+    #  Uses the raw values obtained from self.getcputime() function and calculates CPU load percentages
+    #
+    #  @param     self
     def run(self):
         start = self.getcputime()
 
