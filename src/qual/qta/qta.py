969a757e0bb66e78711c9f2ba8642bf0edd9deaa
atreto <alberto.treto@tklabs.com>
2016-05-26 16:02:45 -0400

    QUAL-110 QTA Application Overview:  * When QTA is created it creates a map{<class_name>:[instances..]} with all the module objects corresponding to each configuration  * Once a message hits, QTA will look for in the __instances of all class the message handler  * If no message handler sens an error, other cases sends the ThaleZMQ response

--- src/qual/qta/qta.py
+++ src/qual/qta/qta.py
@@ -1,6 +1,11 @@
 
-from common.tzmq.ThalesZMQServer import ThalesZMQServer
+from src.common.tzmq.ThalesZMQServer import ThalesZMQServer
+from src.common.classFinder.classFinder import ClassFinder
+from src.common.module.module import Module
+from src.common.gpb.python import *
 
+
+## Map to the
 
 ## Qual Test Application Class
 #
@@ -13,15 +18,33 @@ from common.tzmq.ThalesZMQServer import ThalesZMQServer
 #
 class QualTestApp(ThalesZMQServer):
     ## Constructor
-    #
-    def __init__(self):
-        # FIXME: Requirements don't state what TCP port to use for QTA, so this is arbitrary
-        super(QualTestApp, self).__init__("tcp://*:50001")
+    # @attrib : modInstances The map of {<class>:[<instances>...]}
+    # @attrib : module Classes
+    # @attrib : gpb Classes
+    def __init__(self, ip='0.0.0.0', port=50001):
+        self.__instances = {}
 
-        # TODO: Put GPB class loading (ClassFinder) stuff here
+        #  Address to be bended
+        address = str.format('tcp://{}:{}',ip, port)
+        super(QualTestApp, self).__init__(address=address)
 
-        # TODO: Put module discovery and instance creation here
+        #  All available classes in QUAL modules for QTA,
+        self.__modClasses = ClassFinder(rootPath='qual.modules',
+                                        baseClase=Module)
 
+        #  All available classes in GPB modules for QTA,
+        self.__gpbClasses = ClassFinder(rootPath='qual.modules',
+                                        baseClase=GeneratedProtocolMessageType)
+
+        #  Create instances for each possible configuration
+        for className in self.__modClasses.messageMap.keys():
+            _class = self.__modClasses.getClassByName(className)
+            _config = _class.getConfigurations()
+            for config in _config:
+                if self.__instances.has_key(className):
+                    self.__instances[className] = self.__instances.append(_class(config))
+                else :
+                    self.__instances[className] = list(_class(config))
 
     ## Called by base class when a request is received from a client.
     #
@@ -29,26 +52,29 @@ class QualTestApp(ThalesZMQServer):
     #
     def handleRequest(self, request):
         # Route messages based on type
-
-        # TODO: Use ClassFinder to map request.name to a GPB class and deserialize request.body
-        # If ClassFinder does not find a match, send reply message using:
-        #    self.sendUnsupportedMessageErrorResponse()
-        # If ClassFinder does find a match:
-        #    Create an object of that class:         msgBody = MatchedClass()
-        #    Deserialize the body into that object:  msgBody.ParseFromString(request.serializedBody)
-        #    Update the request with the new object: request.body = msgBody
-
-        # TODO: Route the deserialized message to a module instance
-        # Next look up message class in registered modules list and
-        # pass request to each matching module instance in sequence,
-        # as described in "QAL Module Design" document.
-
-        # TODO: Send reply back over ZMQ
-        # If module instance handles a message, it will return a response message
-        # and you can call this to send the response back over ZMQ:
-        #    self.sendResponse(response)
-        # If no module instance handles a request, call:
-        self.sendUnsupportedMessageErrorResponse()
+        requestClass = self.__gpbClasses.getClassByName(request.name)
+        if requestClass == None:
+            self.sendUnsupportedMessageErrorResponse()
+        else:
+            #  Create the message to pass to the module instances
+            msg = requestClass()
+            msg.ParseFromString(request.serializedBody)
+            #  Update the request
+            request.body = msg
+            #   Get the correct module
+            msgProcessed = False
+            for _module in self.__instances.itervalues():
+                for modObjet in _module:
+                    for msgHandler in modObjet.msgHandlers:
+                        #  if the message class name is the same than one of the instances pass the message
+                        if msgHandler[0].__class__.__name__ == msg.__class__.__name__:
+                            #  Get the ThalesZMQ response object
+                            response = modObjet.msgHandler(msg)
+                            self.SendResponse(response=response)
+                            msgProcessed = True
+            #  If no module instance handled the request, send en error
+            if not msgProcessed:
+                self.sendUnsupportedMessageErrorResponse()
 
 if __name__ == "__main__":
     # Create a QTA instance and start it running
