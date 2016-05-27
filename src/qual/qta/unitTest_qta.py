25132851f070f6bb50d609a2759bfbeae63ed6e2
Jim Burmeister <jim.burmeister@tklabs.com>
2016-05-27 15:38:43 -0400

    QUAL-110: Enhanced QTA unit test with additional tests and ClassFinder lookup of responses

--- src/qual/qta/unitTest_qta.py
+++ src/qual/qta/unitTest_qta.py
@@ -1,7 +1,11 @@
 
+from time import sleep
 from common.tzmq.ThalesZMQClient import ThalesZMQClient
 from common.tzmq.ThalesZMQMessage import ThalesZMQMessage
-from common.gpb.python.CPULoading_pb2 import CPULoadingRequest, CPULoadingResponse
+from common.classFinder.classFinder import ClassFinder
+from common.logger.logger import Logger
+from google.protobuf.message import Message
+from common.gpb.python import CPULoading_pb2
 
 
 ## QTA Tester class
@@ -12,24 +16,94 @@ class QTAClient(ThalesZMQClient):
     def __init__(self):
         super(QTAClient, self).__init__("tcp://localhost:50001")
 
-    @staticmethod
-    def printResponse(response):
-        if response.name == "CPULoadingResponse":
-            cpuResponse = CPULoadingResponse()
-            cpuResponse.ParseFromString(response.serializedBody)
-            print cpuResponse
-        elif response.name == "ErrorMessage":
-            print "Got error message from server"
+        # Set up a ClassFinder for GPB message classes
+        self.gpbClasses = ClassFinder(rootPath='common.gpb.python',
+                                      baseClass=Message)
+        # Set up a logger
+        self.log = Logger(name='Test QTA')
+
+    def sendAndLogResponse(self, request):
+        # Send request and get response
+        response = self.sendRequest(request)
+        self.log.info("Sent %s" % request.name)
+
+        # Look up response type in our ClassFinder
+        responseClass = self.gpbClasses.getClassByName(response.name)
+        if responseClass is None:
+            self.log.warning("Received unknown message class %s" % response.name)
         else:
-            print "Error! Unknown response type"
+            # Deserialize the response body and print it
+            responseBody = responseClass()
+            responseBody.ParseFromString(response.serializedBody)
+            self.log.info("Received %s:\n%s" % (response.name, responseBody))
 
     def test_CPULoading(self):
-        message = CPULoadingRequest()
+        message = CPULoading_pb2.CPULoadingRequest()
 
         ## test REPORT before CPU load
-        message.requestType = CPULoadingRequest.REPORT
-        response = self.sendRequest(ThalesZMQMessage(message))
-        self.printResponse(response)
+        self.log.info("REPORT before CPU load:")
+        message.requestType = CPULoading_pb2.CPULoadingRequest.REPORT
+        request = ThalesZMQMessage(message)
+        self.sendAndLogResponse(request)
+        sleep(3)
+
+        ## test RUN message with default level input and report
+        self.log.info("RUN with default level and report:")
+        message.requestType = CPULoading_pb2.CPULoadingRequest.RUN
+        request = ThalesZMQMessage(message)
+        self.sendAndLogResponse(request)
+        sleep(3)
+
+        ## test REPORT message input after CPU load
+        self.log.info("REPORT after CPU load:")
+        message.requestType = CPULoading_pb2.CPULoadingRequest.REPORT
+        request = ThalesZMQMessage(message)
+        self.sendAndLogResponse(request)
+        sleep(3)
+
+        ## test additional RUN with custom level
+        self.log.info("RUN again with custom level while previous load running:")
+        message.requestType = CPULoading_pb2.CPULoadingRequest.RUN
+        message.level = 50
+        request = ThalesZMQMessage(message)
+        self.sendAndLogResponse(request)
+        sleep(3)
+
+        ## test REPORT message input after additional RUN and custom level
+        self.log.info("REPORT after starting additional load with custom level:")
+        message.requestType = CPULoading_pb2.CPULoadingRequest.REPORT
+        request = ThalesZMQMessage(message)
+        self.sendAndLogResponse(request)
+        sleep(3)
+
+        ## test STOP message input and report
+        self.log.info("STOP and report:")
+        message.requestType = CPULoading_pb2.CPULoadingRequest.STOP
+        request = ThalesZMQMessage(message)
+        self.sendAndLogResponse(request)
+        sleep(3)
+
+        ## test REPORT message input after CPU load stop
+        self.log.info("REPORT after stopping load:")
+        message.requestType = CPULoading_pb2.CPULoadingRequest.REPORT
+        request = ThalesZMQMessage(message)
+        self.sendAndLogResponse(request)
+        sleep(3)
+
+        ## test STOP with no load
+        self.log.info("STOP with no load:")
+        message.requestType = CPULoading_pb2.CPULoadingRequest.STOP
+        request = ThalesZMQMessage(message)
+        self.sendAndLogResponse(request)
+        sleep(3)
+
+        ## test REPORT message input after stopping with no load
+        self.log.info("REPORT after stopping with no load:")
+        message.requestType = CPULoading_pb2.CPULoadingRequest.REPORT
+        request = ThalesZMQMessage(message)
+        self.sendAndLogResponse(request)
+        sleep(3)
+
 
 
 if __name__ == "__main__":
