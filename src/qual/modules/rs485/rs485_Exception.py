4205bbaa41486c9a6542ee84a1be51e5bfae43a7
atreto <alberto.treto@tklabs.com>
2016-06-03 11:31:09 -0400

    QUAL-75 Changed the error level from DEBUG to FATAL

--- src/qual/modules/rs485/rs485_Exception.py
+++ src/qual/modules/rs485/rs485_Exception.py
@@ -7,5 +7,7 @@ from common.logger.logger import Logger
 class RS485ModuleSerialException(ModuleException):
     def __init__(self, port):
         super(RS485ModuleSerialException, self).__init__()
-        self.log = Logger(name='RS-485 Module', level=logging.DEBUG)
-        self.log.error('Unable to open device %s.' % (port,))
+        self.msg = 'Unable to open device %s.' % (port,)
+        self.log = Logger(name='RS-485 Module', level=logging.FATAL)
+        self.log.error(self.msg)
+
