ddf07627a6e9cff0fbbd4bd1abf7a5ebd4028978
Jim Burmeister <jim.burmeister@tklabs.com>
2016-06-07 11:44:08 -0400

    QUAL-111: Changed CPU, Memory Bandwidth, RS-232, and RS-485 modules to use ModuleMessages; renamed RS-232 and Memory Bandwidth for consistency

--- src/qual/modules/cpuLoading/unitTest_CPULoading.py
+++ src/qual/modules/cpuLoading/unitTest_CPULoading.py
@@ -2,85 +2,93 @@ import unittest
 from time import sleep
 
 import CPULoading
-from common.gpb.python import CPULoading_pb2
+from common.gpb.python.CPULoading_pb2 import CPULoadingRequest
 from common.tzmq.ThalesZMQMessage import ThalesZMQMessage
 from common.logger.logger import Logger
+from common.module.modulemsgs import ModuleMessages
 
 # @cond doxygen_unittest
 
+## CPULoading Messages
+class CPULoadingMessages(ModuleMessages):
+    @staticmethod
+    def getMenuTitle():
+        return "CPULoading"
+
+    @staticmethod
+    def getMenuItems():
+        return [("Report",                   CPULoadingMessages.report),
+                ("Run - default load (80%)", CPULoadingMessages.runDefault),
+                ("Run - 50% load",           CPULoadingMessages.run50),
+                ("Stop",                     CPULoadingMessages.stop)]
+
+    @staticmethod
+    def report():
+        message = CPULoadingRequest()
+        message.requestType = CPULoadingRequest.REPORT
+        return message
+
+    @staticmethod
+    def runDefault():
+        message = CPULoadingRequest()
+        message.requestType = CPULoadingRequest.RUN
+        return message
+
+    @staticmethod
+    def run50():
+        message = CPULoadingRequest()
+        message.requestType = CPULoadingRequest.RUN
+        message.level = 50
+        return message
+
+    @staticmethod
+    def stop():
+        message = CPULoadingRequest()
+        message.requestType = CPULoadingRequest.STOP
+        return message
+
 ## CPULoading Unit Test
-#
 class Test_CPULoading(unittest.TestCase):
     def test_basic(self):
-        log = Logger(name='Test CPULoading')
+        log = Logger(name='Test CPU Loading')
         log.info('Running functionality test for CPULoading module:')
+
         self.module = CPULoading.CPULoading()
 
-        message = CPULoading_pb2.CPULoadingRequest()
-        sleep(3)
-
-        ## test REPORT before CPU load
         log.info("REPORT before CPU load:")
-        message.requestType = CPULoading_pb2.CPULoadingRequest.REPORT
-        request = ThalesZMQMessage(message)
-        self.module.msgHandler(request)
+        self.module.msgHandler(ThalesZMQMessage(CPULoadingMessages.report()))
         sleep(3)
 
-        ## test RUN message with default level input and report
         log.info("RUN with default level and report:")
-        message.requestType = CPULoading_pb2.CPULoadingRequest.RUN
-        request = ThalesZMQMessage(message)
-        self.module.msgHandler(request)
+        self.module.msgHandler(ThalesZMQMessage(CPULoadingMessages.runDefault()))
         sleep(3)
 
-        ## test REPORT message input after CPU load
         log.info("REPORT after CPU load:")
-        message.requestType = CPULoading_pb2.CPULoadingRequest.REPORT
-        request = ThalesZMQMessage(message)
-        self.module.msgHandler(request)
+        self.module.msgHandler(ThalesZMQMessage(CPULoadingMessages.report()))
         sleep(3)
 
-        ## test additional RUN with custom level
         log.info("RUN again with custom level while previous load running:")
-        message.requestType = CPULoading_pb2.CPULoadingRequest.RUN
-        message.level = 50
-        request = ThalesZMQMessage(message)
-        self.module.msgHandler(request)
+        self.module.msgHandler(ThalesZMQMessage(CPULoadingMessages.run50()))
         sleep(3)
 
-        ## test REPORT message input after additional RUN and custom level
         log.info("REPORT after starting additional load with custom level:")
-        message.requestType = CPULoading_pb2.CPULoadingRequest.REPORT
-        request = ThalesZMQMessage(message)
-        self.module.msgHandler(request)
+        self.module.msgHandler(ThalesZMQMessage(CPULoadingMessages.report()))
         sleep(3)
 
-        ## test STOP message input and report
         log.info("STOP and report:")
-        message.requestType = CPULoading_pb2.CPULoadingRequest.STOP
-        request = ThalesZMQMessage(message)
-        self.module.msgHandler(request)
+        self.module.msgHandler(ThalesZMQMessage(CPULoadingMessages.stop()))
         sleep(3)
 
-        ## test REPORT message input after CPU load stop
         log.info("REPORT after stopping load:")
-        message.requestType = CPULoading_pb2.CPULoadingRequest.REPORT
-        request = ThalesZMQMessage(message)
-        self.module.msgHandler(request)
+        self.module.msgHandler(ThalesZMQMessage(CPULoadingMessages.report()))
         sleep(3)
 
-        ## test STOP with no load
         log.info("STOP with no load:")
-        message.requestType = CPULoading_pb2.CPULoadingRequest.STOP
-        request = ThalesZMQMessage(message)
-        self.module.msgHandler(request)
+        self.module.msgHandler(ThalesZMQMessage(CPULoadingMessages.stop()))
         sleep(3)
 
-        ## test REPORT message input after stopping with no load
         log.info("REPORT after stopping with no load:")
-        message.requestType = CPULoading_pb2.CPULoadingRequest.REPORT
-        request = ThalesZMQMessage(message)
-        self.module.msgHandler(request)
+        self.module.msgHandler(ThalesZMQMessage(CPULoadingMessages.report()))
         sleep(3)
 
         self.module.terminate()
