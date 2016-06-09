4b2ca8550aa5c080e7eb8e00893419091eda92b0
Christopher Wallace <chris.wallace@tklabs.com>
2016-06-09 10:39:21 -0400

    QUAL-56 : Changed implementation of handler responses and added other small changes

--- src/qual/modules/arinc717/unitTest_arinc717.py
+++ src/qual/modules/arinc717/unitTest_arinc717.py
@@ -1,13 +1,12 @@
 import unittest
 from time import sleep
-
 import arinc717
 from common.gpb.python.ARINC717Frame_pb2 import ARINC717FrameRequest
 from common.tzmq.ThalesZMQMessage import ThalesZMQMessage
 from common.logger.logger import Logger
 from common.module.modulemsgs import ModuleMessages
 
-# @cond doxygen_unittest
+#  @cond doxygen_unittest
 
 ## ARINC717 Messages
 class ARINC717Messages(ModuleMessages):
@@ -25,9 +24,7 @@ class ARINC717Messages(ModuleMessages):
         message.requestType = ARINC717FrameRequest.REPORT
         return message
 
-
 ## ARINC717 Unit Test
-#
 class Test_ARINC717(unittest.TestCase):
     def test_basic(self):
         log = Logger(name='Test ARINC717')
