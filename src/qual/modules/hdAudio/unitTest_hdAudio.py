620434633cdeada8145368e0fe908fe429c0ff26
atreto <alberto.treto@tklabs.com>
2016-06-24 14:04:58 -0400

    QUAL-161 Unit test updated

--- src/qual/modules/hdAudio/unitTest_hdAudio.py
+++ src/qual/modules/hdAudio/unitTest_hdAudio.py
@@ -1,5 +1,5 @@
 import unittest
-from time import sleep
+import time
 
 from common.gpb.python.HDAudio_pb2 import HDAudioRequest, HDAudioResponse
 from common.tzmq.ThalesZMQMessage import ThalesZMQMessage
@@ -52,39 +52,235 @@ class HDAudioMessages(ModuleMessages):
 
 ## HD Audio Unit Test
 class Test_HDAudio(unittest.TestCase):
-    def test_basic(self):
-        log = Logger(name='Test HD Audio')
-        log.info('Running functionality test for HD Audio module:')
-        self.module = HDAudio()
+    ## Static logger instance
+    log = None
 
-        log.info("==== Report before connecting ====")
-        self.module.msgHandler(ThalesZMQMessage(HDAudioMessages.report()))
-        sleep(1)
+    ## Static module instance
+    module = None
 
-        log.info("==== Play one file ====")
-        self.module.msgHandler(ThalesZMQMessage(HDAudioMessages.connectTest1()))
-        sleep(4)
+    ## Setup for the HDAudio test cases
+    # This is run only once before running any test cases
+    @classmethod
+    def setUpClass(cls):
+        # Create a logger so we can add details to a multi-step test case
+        cls.log = Logger(name='Test HD Audio')
+        cls.log.info('++++ Setup before HD Audio module unit tests ++++')
+        # Create the module
+        cls.module = HDAudio()
+        # Uncomment this if you don't want to see module debug messages
+        # cls.module.log.setLevel(logger.INFO)
 
-        log.info("==== Play a second file ====")
-        self.module.msgHandler(ThalesZMQMessage(HDAudioMessages.connectTest2()))
-        sleep(4)
+    ## Teardown when done with HDAudio test cases
+    # This is run only once when we're done with all test cases
+    @classmethod
+    def tearDownClass(cls):
+        cls.log.info("++++ Teardown after HD Audio module unit tests ++++")
+        cls.module.terminate()
 
-        log.info("==== Stop playing ====")
-        self.module.msgHandler(ThalesZMQMessage(HDAudioMessages.disconnect()))
-        sleep(1)
+    ## Test setup
+    # This is run before each test case; we use it to make sure we
+    # start each test case with the module in a known state
+    def setUp(self):
+        log = self.__class__.log
+        module = self.__class__.module
+        log.info("==== Reset module state ====")
+        module.msgHandler(ThalesZMQMessage(HDAudioMessages.disconnect()))
 
-        log.info("==== Play again ====")
-        self.module.msgHandler(ThalesZMQMessage(HDAudioMessages.connectTest1()))
-        sleep(4)
+    ## Valid Test case: Send a REPORT msg
+    # Asserts:
+    #       appState == STOPPED
+    #       source == ""
+    #       volume == 100
+    #       ---------------------
+    def test_Report(self):
+        log = self.__class__.log
+        module = self.__class__.module
 
-        log.info("==== Stop playing ====")
-        self.module.msgHandler(ThalesZMQMessage(HDAudioMessages.disconnect()))
-        sleep(1)
+        # Create message
+        message = HDAudioRequest()
+        message.requestType = HDAudioRequest.REPORT
 
-        log.info("Terminating module...")
-        self.module.terminate()
+        log.info("**** Test case: REPORT message ****")
 
-        pass
+        response = module.msgHandler(ThalesZMQMessage(message))
+        # asserts
+        self.assertEqual(response.name, "HDAudioResponse")
+        self.assertEqual(response.body.appState, HDAudioResponse.DISCONNECTED)
+        self.assertEqual(response.body.source, '')
+
+    ## Valid Test case: Send a CONNECT, REPORT and DISCONNECT msgs
+    # Asserts:
+    #       appState == CONNECTED
+    #       source == "braindamage.wav"
+    #       volume == 100
+    #       ---------------------
+    #       appState == CONNECTED
+    #       source == "braindamage.wav"
+    #       volume == 100
+    #       ---------------------
+    #       appState == DISCONNECTED
+    #       source == "braindamage.wav"
+    #       volume == 100
+    #       ---------------------
+    def test_ConnectReportDisconnect_Vol100(self):
+        log = self.__class__.log
+        module = self.__class__.module
+
+        log.info("**** Test case: CONNECT, REPORT and DISCONNECT message ****")
+        # Connect
+        response = module.msgHandler(ThalesZMQMessage(HDAudioMessages.connectTest1()))
+        # asserts
+        self.assertEqual(response.name, "HDAudioResponse")
+        self.assertEqual(response.body.appState, HDAudioResponse.CONNECTED)
+        self.assertEqual(response.body.source, 'braindamage.wav')
+        self.assertEqual(response.body.volume, 100)
+        # Report
+        response = module.msgHandler(ThalesZMQMessage(HDAudioMessages.report()))
+        self.assertEqual(response.name, "HDAudioResponse")
+        self.assertEqual(response.body.appState, HDAudioResponse.CONNECTED)
+        self.assertEqual(response.body.source, 'braindamage.wav')
+        self.assertEqual(response.body.volume, 100)
+        # Allow the audio for a while
+        time.sleep(5)
+        # Disconnect
+        response = module.msgHandler(ThalesZMQMessage(HDAudioMessages.disconnect()))
+        self.assertEqual(response.name, "HDAudioResponse")
+        self.assertEqual(response.body.appState, HDAudioResponse.DISCONNECTED)
+        self.assertEqual(response.body.source, '')
+        self.assertEqual(response.body.volume, 100)
+
+    ## Valid Test case: Send a CONNECT, REPORT and DISCONNECT msgs
+    # Asserts:
+    #       appState == CONNECTED
+    #       source == "comfortablynumb.wav"
+    #       volume == 50
+    #       ---------------------
+    #       appState == CONNECTED
+    #       source == "comfortablynumb.wav"
+    #       volume == 50
+    #       ---------------------
+    #       appState == DISCONNECTED
+    #       source == "comfortablynumb.wav"
+    #       volume == 50
+    #       ---------------------
+    def test_ConnectReportDisconnect_Vol50(self):
+        log = self.__class__.log
+        module = self.__class__.module
+
+        log.info("**** Test case: CONNECT, REPORT and DISCONNECT message ****")
+        # Connect
+        response = module.msgHandler(ThalesZMQMessage(HDAudioMessages.connectTest2()))
+        # asserts
+        self.assertEqual(response.name, "HDAudioResponse")
+        self.assertEqual(response.body.appState, HDAudioResponse.CONNECTED)
+        self.assertEqual(response.body.source, 'comfortablynumb.wav')
+        self.assertEqual(response.body.volume, 50)
+        # Report
+        response = module.msgHandler(ThalesZMQMessage(HDAudioMessages.report()))
+        self.assertEqual(response.name, "HDAudioResponse")
+        self.assertEqual(response.body.appState, HDAudioResponse.CONNECTED)
+        self.assertEqual(response.body.source, 'comfortablynumb.wav')
+        self.assertEqual(response.body.volume, 50)
+        # Allow the audio for a while
+        time.sleep(5)
+        # Disconnect
+        response = module.msgHandler(ThalesZMQMessage(HDAudioMessages.disconnect()))
+        self.assertEqual(response.name, "HDAudioResponse")
+        self.assertEqual(response.body.appState, HDAudioResponse.DISCONNECTED)
+        self.assertEqual(response.body.source, '')
+        self.assertEqual(response.body.volume, 50)
+
+    ## Valid Test case: Send a CONNECT, DISCONNECT, CONNECT and DISCONNECT msgs
+    # Asserts:
+    #       appState == CONNECTED
+    #       source == "braindamage.wav"
+    #       volume == 100
+    #       ---------------------
+    #       appState == DISCONNECTED
+    #       source == "braindamage.wav"
+    #       volume == 100
+    #       ---------------------
+    #       appState == CONNECTED
+    #       source == "comfortablynumb.wav"
+    #       volume == 50
+    #       ---------------------
+    #       appState == DISCONNECTED
+    #       source == "comfortablynumb.wav"
+    #       volume == 50
+    #       ---------------------
+    def test_ConnectReportDisconnect_Vol50(self):
+        log = self.__class__.log
+        module = self.__class__.module
+
+        log.info("**** Test case: CONNECT, DISCONNECT, CONNECT and DISCONNECT message ****")
+
+        # Connect
+        response = module.msgHandler(ThalesZMQMessage(HDAudioMessages.connectTest1()))
+        # asserts
+        self.assertEqual(response.name, "HDAudioResponse")
+        self.assertEqual(response.body.appState, HDAudioResponse.CONNECTED)
+        self.assertEqual(response.body.source, 'braindamage.wav')
+        self.assertEqual(response.body.volume, 100)
+        # Allow the audio for a while
+        time.sleep(5)
+        # Disconnect
+        response = module.msgHandler(ThalesZMQMessage(HDAudioMessages.disconnect()))
+        self.assertEqual(response.name, "HDAudioResponse")
+        self.assertEqual(response.body.appState, HDAudioResponse.DISCONNECTED)
+        self.assertEqual(response.body.source, '')
+        self.assertEqual(response.body.volume, 100)
+        # Connect
+        response = module.msgHandler(ThalesZMQMessage(HDAudioMessages.connectTest2()))
+        # asserts
+        self.assertEqual(response.name, "HDAudioResponse")
+        self.assertEqual(response.body.appState, HDAudioResponse.CONNECTED)
+        self.assertEqual(response.body.source, 'comfortablynumb.wav')
+        self.assertEqual(response.body.volume, 50)
+        # Allow the audio for a while
+        time.sleep(5)
+        # Disconnect
+        response = module.msgHandler(ThalesZMQMessage(HDAudioMessages.disconnect()))
+        self.assertEqual(response.name, "HDAudioResponse")
+        self.assertEqual(response.body.appState, HDAudioResponse.DISCONNECTED)
+        self.assertEqual(response.body.source, '')
+        self.assertEqual(response.body.volume, 50)
+
+    ## Invalid Test case: Send a wrong volume
+    # Asserts:
+    #       --------------------
+    def test_WrongVolume(self):
+        log = self.__class__.log
+        module = self.__class__.module
+
+        log.info("**** Test case: Wrong Volume ****")
+        # Message
+        message = HDAudioRequest()
+        message.requestType = HDAudioRequest.CONNECT
+        message.source = 'braindamage.wav'
+        message.volume = 120
+
+        # Connect
+        response = module.msgHandler(ThalesZMQMessage(message))
+        self.assertEqual(response.body.appState, HDAudioResponse.CONNECTED)
+        self.assertEqual(response.body.volume, 100)
+
+    ## Invalid Test case: Send a source path
+    # Asserts:
+    #       --------------------
+    def test_WrongPath(self):
+        log = self.__class__.log
+        module = self.__class__.module
+
+        log.info("**** Test case: Wrong source path ****")
+        # Message
+        message = HDAudioRequest()
+        message.requestType = HDAudioRequest.CONNECT
+        message.source = 'anotherbrickinthewall.wav'
+        message.volume = 100
+
+        # Connect
+        response = module.msgHandler(ThalesZMQMessage(message))
+        self.assertEqual(response.body.appState, HDAudioResponse.DISCONNECTED)
 
 if __name__ == '__main__':
     unittest.main()
