76218ea9ce5d469119406f4b2beb3e8c5b90bd2d
Christopher Wallace <chris.wallace@tklabs.com>
2016-07-08 13:03:02 -0400

    QUAL-187 : Updated unit test with more info logging

--- src/qual/modules/ssd/unitTest_ssd.py
+++ src/qual/modules/ssd/unitTest_ssd.py
@@ -106,14 +106,16 @@ class Test_SSD(unittest.TestCase):
         readBandTotal = 0
 
         log.info("**** Test case: Send a series of messages to confirm Start, Stop, and Report functionality ****")
-
+        log.info("==== Run SSD module ====")
         response = module.msgHandler(ThalesZMQMessage(SSDMessages.run()))
         # Asserts
         self.assertEqual(response.name, "SSDResponse")
         self.assertEqual(response.body.state, SSDResponse.RUNNING)
+        log.info("==== Sleeping for 40 seconds to allow 1GB file to be written ====")
         # Sleep to allow 1GB fio file to be written to drive on first run
         sleep(40)
 
+        log.info("==== Have module Report to verify readBandwidth activity ====")
         # readBandwidth is 0.0 occasionally, so we collect a few times to make sure fio is active
         for x in xrange(3):
             response = module.msgHandler(ThalesZMQMessage(SSDMessages.report()))
@@ -125,27 +127,33 @@ class Test_SSD(unittest.TestCase):
 
         self.assertGreater(readBandTotal, 0.0)
 
+        log.info("==== Stop SSD module ====")
         response = module.msgHandler(ThalesZMQMessage(SSDMessages.stop()))
         # Asserts
         self.assertEqual(response.name, "SSDResponse")
         self.assertEqual(response.body.state, SSDResponse.STOPPED)
+        log.info("==== Sleeping for 3 seconds to allow fio to stop ====")
         # Sleep to allow fio time to stop running
         sleep(3)
 
+        log.info("==== Have module Report to verify readBandwidth is 0.0 ====")
         response = module.msgHandler(ThalesZMQMessage(SSDMessages.report()))
         # Asserts
         self.assertEqual(response.name, "SSDResponse")
         self.assertEqual(response.body.state, SSDResponse.STOPPED)
         self.assertEqual(response.body.readBandwidth, 0.0)
 
+        log.info("==== Re-run SSD module ====")
         response = module.msgHandler(ThalesZMQMessage(SSDMessages.run()))
         # Asserts
         self.assertEqual(response.name, "SSDResponse")
         self.assertEqual(response.body.state, SSDResponse.RUNNING)
         self.assertEqual(response.body.readBandwidth, 0.0)
+        log.info("==== Sleeping for 10 seconds to allow fio to start ====")
         # Sleep to allow fio time start running
         sleep(10)
 
+        log.info("==== Have module Report to verify readBandwidth activity ====")
         # readBandwidth is 0.0 occasionally, so we collect a few times to make sure fio is active
         for x in xrange(3):
             response = module.msgHandler(ThalesZMQMessage(SSDMessages.report()))
@@ -157,13 +165,16 @@ class Test_SSD(unittest.TestCase):
 
         self.assertGreater(readBandTotal, 0.0)
 
+        log.info("==== Stop SSD module ====")
         response = module.msgHandler(ThalesZMQMessage(SSDMessages.stop()))
         # Asserts
         self.assertEqual(response.name, "SSDResponse")
         self.assertEqual(response.body.state, SSDResponse.STOPPED)
+        log.info("==== Sleep for 3 seconds to allow fio to stop ====")
         # Sleep to allow fio time to stop running
         sleep(3)
 
+        log.info("==== Stop SSD while already STOPPED ====")
         response = module.msgHandler(ThalesZMQMessage(SSDMessages.stop()))
         # Asserts
         self.assertEqual(response.name, "SSDResponse")
