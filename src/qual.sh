ee6e888607119fbc9fd6005aa3ecf4284ddd38cb
Jim Burmeister <jim.burmeister@tklabs.com>
2016-07-18 12:54:09 -0400

    QUAL-192: Fixed RPM command

--- src/qual.sh
+++ src/qual.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Qual package version is: `rpm -q --queryformat='%{VERSION}'`"
+echo "Qual package version is: `rpm -q --queryformat='%{VERSION}' qual`"
 cd /thales/qual/src/
 PYTHONPATH=`pwd` python qual/qta/qta.py
