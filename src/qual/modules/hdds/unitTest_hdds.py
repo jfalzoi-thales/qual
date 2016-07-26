54b9fb7844e03d6596fe665e64148d19f19df80b
Christopher Wallace <chris.wallace@tklabs.com>
2016-07-26 14:06:32 -0400

    QUAL-233 : Updated HDDS module to agree with updated ICD

--- src/qual/modules/hdds/unitTest_hdds.py
+++ src/qual/modules/hdds/unitTest_hdds.py
@@ -1,6 +1,6 @@
 import unittest
 import hdds
-from common.gpb.python.HDDS_API_pb2 import GetReq, SetReq, FAILURE_INVALID_KEY
+from common.gpb.python.HDDS_pb2 import HostDomainDeviceServiceRequest
 from common.tzmq.ThalesZMQMessage import ThalesZMQMessage
 from common.logger.logger import Logger
 from common.module.modulemsgs import ModuleMessages
@@ -15,46 +15,38 @@ class HDDSMessages(ModuleMessages):
 
     @staticmethod
     def getMenuItems():
-        return [("Get single value",     HDDSMessages.getSingle),
-                ("Get multiple values",  HDDSMessages.getMultiple),
-                ("Set single value",     HDDSMessages.setSingle)]
+        return [("Get single value", HDDSMessages.get),
+                ("Set single value", HDDSMessages.set)]
 
     @staticmethod
-    def getSingle():
-        message = GetReq()
-        message.key.append("external_pins.output.pin_a6")
+    def get():
+        message = HostDomainDeviceServiceRequest()
+        message.requestType = HostDomainDeviceServiceRequest.GET
+        message.key = "external_pins.output.pin_a6"
         return message
 
     @staticmethod
-    def getMultiple():
-        message = GetReq()
-        message.key.append("external_pins.output.pin_a6")
-        message.key.append("external_pins.output.pin_b6")
-        message.key.append("external_pins.output.pin_c6")
-        return message
-
-    @staticmethod
-    def setSingle():
-        message = SetReq()
-        prop = message.values.add()
-        prop.key = "external_pins.output.pin_a6"
-        prop.value = "HIGH"
+    def set():
+        message = HostDomainDeviceServiceRequest()
+        message.requestType = HostDomainDeviceServiceRequest.SET
+        message.key = "external_pins.output.pin_a6"
+        message.value = "HIGH"
         return message
 
     @staticmethod
     def getBogus():
-        message = GetReq()
-        message.key.append("bogus_key")
+        message = HostDomainDeviceServiceRequest()
+        message.requestType = HostDomainDeviceServiceRequest.GET
+        message.key = "bogus_key"
         return message
 
     @staticmethod
     def setBogus():
-        message = SetReq()
-        prop = message.values.add()
-        prop.key = "bogus_key"
-        prop.value = "x"
+        message = HostDomainDeviceServiceRequest()
+        message.requestType = HostDomainDeviceServiceRequest.SET
+        message.key = "bogus_key"
+        message.value = "x"
         return message
-
 
 ## HDDS Unit Test
 class Test_HDDS(unittest.TestCase):
@@ -77,77 +69,66 @@ class Test_HDDS(unittest.TestCase):
         #cls.module.log.setLevel("DEBUG")
 
     ## Test case: Try to get an invalid key
-    # Should return a GetResp with success=False, error_code=FAILURE_INVALID_KEY
+    # Asserts:
+    #       success == False
+    #       key     == "bogus_key"
+    #       value   == ""
     def test_getInvalidKey(self):
         log = self.__class__.log
         module = self.__class__.module
 
         log.info("**** Test case: Get invalid key ****")
         response = module.msgHandler(ThalesZMQMessage(HDDSMessages.getBogus()))
-        self.assertEqual(response.name, "GetResp")
-        self.assertEqual(len(response.body.values), 1)
-        self.assertEqual(response.body.values[0].success, False)
-        self.assertEqual(response.body.values[0].error.error_code, FAILURE_INVALID_KEY)
+        self.assertEqual(response.body.success, False)
+        self.assertEqual(response.body.key, "bogus_key")
+        self.assertEqual(response.body.value, "")
         log.info("==== Test complete ====")
 
     ## Test case: Try to set an invalid key
-    # Should return a SetResp with success=False, error_code=FAILURE_INVALID_KEY
+    # Asserts:
+    #       success == False
+    #       key     == "bogus_key"
+    #       value   == ""
     def test_setInvalidKey(self):
         log = self.__class__.log
         module = self.__class__.module
 
         log.info("**** Test case: Set invalid key ****")
         response = module.msgHandler(ThalesZMQMessage(HDDSMessages.setBogus()))
-        self.assertEqual(response.name, "SetResp")
-        self.assertEqual(len(response.body.values), 1)
-        self.assertEqual(response.body.values[0].success, False)
-        self.assertEqual(response.body.values[0].error.error_code, FAILURE_INVALID_KEY)
+        self.assertEqual(response.body.success, False)
+        self.assertEqual(response.body.key, "bogus_key")
+        self.assertEqual(response.body.value, "")
         log.info("==== Test complete ====")
 
-    ## Test case: Try to get a single value
-    # Should return a GetResp with success=true and nonempty value
-    def test_getSingle(self):
+    ## Test case: Try to get a single value# Asserts:
+    #       success == True
+    #       key     == "external_pins.output.pin_a6"
+    #       value   != ""
+    def test_get(self):
         log = self.__class__.log
         module = self.__class__.module
 
         log.info("**** Test case: Get single ****")
-        response = module.msgHandler(ThalesZMQMessage(HDDSMessages.getSingle()))
-        self.assertEqual(response.name, "GetResp")
-        self.assertEqual(len(response.body.values), 1)
-        self.assertEqual(response.body.values[0].success, True)
-        self.assertEqual(response.body.values[0].keyValue.key, "external_pins.output.pin_a6")
-        self.assertNotEqual(response.body.values[0].keyValue.value, "")
+        response = module.msgHandler(ThalesZMQMessage(HDDSMessages.get()))
+        self.assertEqual(response.body.success, True)
+        self.assertEqual(response.body.key, "external_pins.output.pin_a6")
+        self.assertNotEqual(response.body.value, "")
         log.info("==== Test complete ====")
 
-    ## Test case: Try to set a single value
-    # Should return a SetResp with GetResp with success=true and value equal to what we set
-    def test_setSingle(self):
+    ## Test case: Try to set a single value# Asserts:
+    #       success == True
+    #       key     == "external_pins.output.pin_a6"
+    #       value   == "HIGH"
+    def test_set(self):
         log = self.__class__.log
         module = self.__class__.module
 
         log.info("**** Test case: Set value ****")
-        response = module.msgHandler(ThalesZMQMessage(HDDSMessages.setSingle()))
-        self.assertEqual(response.name, "SetResp")
-        self.assertEqual(len(response.body.values), 1)
-        self.assertEqual(response.body.values[0].success, True)
-        self.assertEqual(response.body.values[0].keyValue.key, "external_pins.output.pin_a6")
-        self.assertEqual(response.body.values[0].keyValue.value, "HIGH")
-
-    ## Test case: Try to get multiple values
-    # Should return a GetResp with success=true for each value
-    def test_getMultiple(self):
-        log = self.__class__.log
-        module = self.__class__.module
-
-        log.info("**** Test case: Get multiple ****")
-        response = module.msgHandler(ThalesZMQMessage(HDDSMessages.getMultiple()))
-        self.assertEqual(response.name, "GetResp")
-        self.assertEqual(len(response.body.values), 3)
-        self.assertEqual(response.body.values[0].success, True)
-        self.assertEqual(response.body.values[1].success, True)
-        self.assertEqual(response.body.values[2].success, True)
+        response = module.msgHandler(ThalesZMQMessage(HDDSMessages.set()))
+        self.assertEqual(response.body.success, True)
+        self.assertEqual(response.body.key, "external_pins.output.pin_a6")
+        self.assertEqual(response.body.value, "HIGH")
         log.info("==== Test complete ====")
-
 
 if __name__ == '__main__':
     unittest.main()
