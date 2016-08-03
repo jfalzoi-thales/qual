131e01b334f8e10d5bc8d5eca3b9aae51298a480
Christopher Wallace <chris.wallace@tklabs.com>
2016-08-03 11:52:30 -0400

    QUAL-247 : Added ARINC485 passthrough module

---
+++ src/qual/modules/arinc485/arinc485.py
@@ -0,0 +1,34 @@
+from common.tzmq.ThalesZMQClient import ThalesZMQClient
+from common.tzmq.ThalesZMQMessage import ThalesZMQMessage
+from common.gpb.python.ARINC485_pb2 import ARINC485Request, ARINC485Response
+from common.module.module import Module
+
+## ARINC485 Module Class
+class ARINC485(Module):
+    ## Constructor
+    #  @param   self
+    #  @param   config  Configuration for this module instance
+    def __init__(self, config = None):
+        #  Initializes parent class
+        super(ARINC485, self).__init__(config)
+        ## Connection to QTA running on the IFE VM
+        self.ifeVmQtaClient = ThalesZMQClient("tcp://localhost:50003", log=self.log)
+        #  Add handler to available message handlers
+        self.addMsgHandler(ARINC485Request, self.handler)
+
+    ## Handles incoming messages
+    #  Receives tzmq request and passes it to the QTA running on the IFE VM
+    #  @param   self
+    #  @param   msg   tzmq format message
+    #  @return  ARINC485Response object
+    def handler(self, msg):
+        response = ARINC485Response()
+        ifeVmQtaResponse = self.ifeVmQtaClient.sendRequest(msg)
+
+        if ifeVmQtaResponse.name == "ARINC485Response":
+            return ifeVmQtaResponse
+        else:
+            self.log.error("Unexpected response from IFE VM ARINC485: %s" % ifeVmQtaResponse.name)
+            response.state = ARINC485Response.STOPPED
+
+        return ThalesZMQMessage(response)
