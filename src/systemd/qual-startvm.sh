012d085ba131e2c4c8ac9128bdcf0fdfddb3a163
Jim Burmeister <jim.burmeister@tklabs.com>
2016-08-03 13:07:51 -0400

    QUAL-248: Added qual-startvm scripts

--- src/systemd/qual-startvm.sh
+++ src/systemd/qual-startvm.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-echo "THIS IS A PLACEHOLDER"
+
+# Generate a config file for libvirt
+/thales/host/appliances/genvmconfig /opt/thales/mps/livecd-mps-guest-*.vm.qcow2 > /tmp/qual_guest.xml
+
+# Start the VM
+virsh create /tmp/qual_guest.xml
