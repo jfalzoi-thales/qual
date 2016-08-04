620ce6e3e87222f24c2023e3ee44ceb100378083
Jim Burmeister <jim.burmeister@tklabs.com>
2016-08-04 13:45:15 -0400

    QUAL-248: Try to get around security error starting vm

--- src/systemd/qual-startvm.sh
+++ src/systemd/qual-startvm.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sleep 2
+
 # Start virtual network
-virsh net-create /etc/libvirt/qemu/networks/default.xml
+#virsh net-create /etc/libvirt/qemu/networks/default.xml
 
 # Generate a config file for libvirt
-/thales/host/appliances/genvmconfig /opt/thales/mps/livecd-mps-guest-*.vm.qcow2 > /tmp/qual_guest.xml
+/thales/host/appliances/genvmconfig /opt/thales/mps/livecd-mps-guest-*.vm.qcow2 > /thales/qual/qual_guest.xml
 
 # Start the VM
-virsh create /tmp/qual_guest.xml
+virsh create /thales/qual/qual_guest.xml
