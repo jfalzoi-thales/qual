e2c54de31ceeb3a0708bff0511fdaa93835b3ad5
Jim Burmeister <jim.burmeister@tklabs.com>
2016-08-19 13:04:17 -0400

    QUAL-267: Use loopback instead of IP addresses for analog audio

--- src/qual/ifeModules/analogAudio/pavaTest.sh
+++ src/qual/ifeModules/analogAudio/pavaTest.sh
@@ -6,16 +6,21 @@ if [ "$#" -lt 3 ]; then
 fi
 
 if [ "$3" == "-k" ] && [ "$#" -eq 5 ]; then
-    echo "some pin disabled"
+    echo "$2 $4 disabled"
     exit 0    
 fi
 
 if [ "$3" == "-a" ] && [ "$#" -eq 6 ]; then
-    echo "some program succeeded"
     sleep 1
+    echo "$2 program succeeded"
+    exit 0
+fi
+
+if [ "$2" == "loopback" ] && [ "$#" -eq 6 ]; then
+    sleep 2
+    echo "Loopback PA $4 to VA $6 succeeded"
     exit 0
 fi
 
 echo "ERROR ERROR ERROR"
 exit 1
-
