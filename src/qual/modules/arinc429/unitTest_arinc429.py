579142abbe9f18a3c041829549e650a1bbd9f5ac
atreto <alberto.treto@tklabs.com>
2016-09-28 11:08:44 -0400

    QUAL-384 Added Qual GUI set up unit test

--- src/qual/modules/arinc429/unitTest_arinc429.py
+++ src/qual/modules/arinc429/unitTest_arinc429.py
@@ -28,7 +28,8 @@ class ARINC429Messages(ModuleMessages):
                 ("Connect all inputs to output 3", ARINC429Messages.connectInAllOut3),
                 ("Disconnect input 1",             ARINC429Messages.disconnectIn1),
                 ("Disconnect input 3",             ARINC429Messages.disconnectIn3),
-                ("Disconnect all inputs",          ARINC429Messages.disconnectAll)]
+                ("Disconnect all inputs",          ARINC429Messages.disconnectAll),
+                ("Qual GUI set up",                ARINC429Messages.guiSetUp)]
 
     @staticmethod
     def reportIn1():
@@ -119,6 +120,25 @@ class ARINC429Messages(ModuleMessages):
         message.sink = "ARINC_429_RX2"
         message.source = "ARINC_429_BOGUS"
         return message
+
+    @staticmethod
+    def guiSetUp():
+        # MAP according to the requirements
+        list = [
+            ('ARINC_429_TX1', 'ARINC_429_RX1'),
+            ('ARINC_429_TX2', 'ARINC_429_RX2'),
+            ('ARINC_429_TX2', 'ARINC_429_RX3'),
+            ('ARINC_429_TX2', 'ARINC_429_RX4'),
+            ('ARINC_429_TX3', 'ARINC_429_RX5'),
+            ('ARINC_429_TX4', 'ARINC_429_RX6'),
+            ('ARINC_429_TX4', 'ARINC_429_RX7'),
+            ]
+        for connection in list:
+            message = ARINC429Request()
+            message.requestType = ARINC429Request.CONNECT
+            message.sink = connection[1]
+            message.source = connection[0]
+            yield message
 
     ## Constructor (not used)
     #  @param     self
@@ -399,6 +419,35 @@ class Test_ARINC429(unittest.TestCase):
             self.assertEqual(ARINC429Stat.conState, ARINC429Response.DISCONNECTED)
             self.assertEqual(ARINC429Stat.source, "")
         log.info("==== Test complete ====")
+
+    ## Test case: Qual GUI set up
+    #  test map:
+    #       TX1 -> RX1
+    #       TX2 -> RX2, RX3, RX4
+    #       TX3 -> RX5
+    #       TX4 -> RX6, RX7
+    def test_QualGUISetUp(self):
+        log = self.__class__.log
+        module = self.__class__.module
+
+        numInputs = 1
+
+        log.info("==== Report on all inputs before connect ====")
+        response = module.msgHandler(ThalesZMQMessage(ARINC429Messages.reportAll()))
+        self.assertEqual(response.name, "ARINC429Response")
+        self.assertEqual(len(response.body.status), 8)
+        for ARINC429Stat in response.body.status:
+            # All channels should be disconnected
+            self.assertEqual(ARINC429Stat.conState, ARINC429Response.DISCONNECTED)
+            self.assertEqual(ARINC429Stat.source, "")
+
+        log.info('**** Test case: Test use of the "Qual GUI Set Up" parameter ****')
+        for message in ARINC429Messages.guiSetUp():
+            sleep(3)
+            log.info("==== %s -> %s ====" % (message.source[-3:], message.sink[-3:]))
+            response = module.msgHandler(ThalesZMQMessage(message))
+            self.assertEqual(response.name, "ARINC429Response")
+            self.assertEqual(len(response.body.status), numInputs)
 
 if __name__ == '__main__':
     unittest.main()
